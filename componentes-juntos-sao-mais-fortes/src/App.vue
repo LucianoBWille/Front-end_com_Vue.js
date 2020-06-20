f40a141e1b7970c04ea024d96c3796204df9c15c
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-20 16:26:46 -0300

    Introdução ao uso de componentes

---
+++ componentes-juntos-sao-mais-fortes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <LVPerfil></LVPerfil>
+    <hr>
+    <LVBusca></LVBusca>
+  </div>
+</template>
+
+<script>
+import LVPerfil from './components/LVPerfil.vue'
+import LVBusca from './components/LVBusca.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LVPerfil,
+    LVBusca,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
