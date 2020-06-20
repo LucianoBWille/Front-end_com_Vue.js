ccdd59aec3df2b3d92eb52a826358bbcf454be53
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-20 19:00:18 -0300

    Finalizado o teste do capítulo 6

---
+++ 6-1_teste_seu_avanco/src/components/feed.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <postagem v-for="post in posts" :key="post" :conteudo="post"></postagem>
+    </div>
+</template>
+
+<script>
+    import postagem from "./postagem";
+
+    export default {
+        name: 'lw-feed',
+        components: {
+            postagem
+        },
+        data () {
+            return {
+                posts: [
+                    {
+                        title: 'alguma coisa',
+                        paragrafos: [
+                            'p1',
+                            'p2',
+                            'p3',
+                        ]
+                    },
+                    {
+                        title: 'alguma coisa',
+                        paragrafos: [
+                            'p1',
+                            'p2',
+                            'p3',
+                        ]
+                    },
+                    {
+                        title: 'alguma coisa',
+                        paragrafos: [
+                            'p1',
+                            'p2',
+                            'p3',
+                        ]
+                    },
+                    {
+                        title: 'alguma coisa',
+                        paragrafos: [
+                            'p1',
+                            'p2',
+                            'p3',
+                        ]
+                    },
+                ]
+            }
+        },
+        methods:{
+
+        },
+        //filters:
+        //computed:
+        //watch:
+    }
+</script>
+
+<style>
+</style>
---
+++ 6-1_teste_seu_avanco/src/components/postagem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        
+            <h1 @click="exibeParagrafo = !exibeParagrafo">{{conteudo.title}}</h1>
+        
+        <div v-if="exibeParagrafo">
+            <p v-for="(paragrafo, index) in conteudo.paragrafos" :key="index">{{paragrafo}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'lw-post',
+        props: ['conteudo'],
+        data () {
+            return {
+                exibeParagrafo: false,
+            }
+        },
+        //methods:
+        //filters:
+        //computed:
+        //watch:
+    }
+</script>
+
+<style>
+</style>
