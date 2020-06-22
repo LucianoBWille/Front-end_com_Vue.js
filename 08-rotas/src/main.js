5ee741c0dd3483796c7792d00cff56c9e2ac100a
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-22 13:07:51 -0300

    criando rotas

--- 08-rotas/src/main.js
+++ 08-rotas/src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
+import VueRouter from 'vue-router'
+import Inicial from "./components/Inicial.vue"
+import Usuarios from "./components/Usuarios.vue"
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  routes: [
+    {path: '/', component:Inicial},
+    {path: '/usuarios', component:Usuarios},
+  ]
 });
 
 new Vue({
-  el: '#App',
   router,
   render: h => h(App),
 }).$mount('#app')
