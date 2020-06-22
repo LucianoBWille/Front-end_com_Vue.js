2466e6df583500d3114080728061331d359e21cf
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-22 12:45:09 -0300

    Configurando o vue-router

---
+++ 08-rotas/src/components/Inicial.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Inicial</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'lv-inicial'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 08-rotas/src/components/Notificacao.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Notificacao</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'lv-notificacao'
+    }
+</script>
+
+<style scoped>
+
+</style>
