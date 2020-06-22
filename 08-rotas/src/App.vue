d4823f4d1a2007082e7d699f1f55148f21d832b1
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-22 17:55:03 -0300

    Trocando de página

--- 08-rotas/src/App.vue
+++ 08-rotas/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link to='/'>Inicial</router-link>          
+    <router-link to='/usuarios'>Usuários</router-link>
     <router-view></router-view>
   </div>
 </template>
